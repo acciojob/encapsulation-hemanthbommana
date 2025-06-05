ffb608fa4961c5e479f3342464b295eb04c8d5be
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2025-06-05 06:24:56 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private name;
+
+    public String getter(){
+        return this.name;
+    }
+    public void setter(String name){
+        this.name = name
+    }
 }
