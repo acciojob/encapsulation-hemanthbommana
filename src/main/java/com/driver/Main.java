f3b849574b66809954707fc3cd912cf2326c19ec
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2025-06-05 06:27:28 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  RWOnly var = new RWOnly();
+  System.out.println(var.name);
+  var.name = "Hemanth";
+  System.out.println(var.getter());
+  var.setter("Hemanth");
 }
